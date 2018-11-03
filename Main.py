bf2d03cda9d516013ce6fd02b84f92a70a6fb103
aaalonzo <aalonzo@utexas.edu>
2018-11-03 12:38:16 -0500

    pushed latest changes

--- Main.py
+++ Main.py
@@ -2,7 +2,7 @@ from tkinter import *
 from Questions import Questions
 
 VERSION = 0.1
-DEF_NUM_QUESTIONS = 3
+DEF_NUM_QUESTIONS = 2
 DEFAULT_TRIES = 3
 
 #global variables for the score and the tries for the duration of the game
@@ -39,6 +39,7 @@ def clear_textbox(text):
 def give_strike(score_w):
 	global global_score
 	global global_tries
+	global begun
 
 	global_tries -= 1
 
@@ -58,11 +59,18 @@ def intro_sequence(text):
 	send_outputln(text, "Welcome to version " + str(VERSION) + " of our trivia game!  Type \"begin\" to begin, or \"quit\" to quit")
 
 def load_next_question(master, score_w, text):
+	global global_score
 	global global_q_counter
+
 	clear_textbox(text)
+
+	global_score += 10
 	global_q_counter += 1 
 	current_q.getSpecificQuestion(global_q_counter)
-	send_outputln(str(global_q_counter) + str(current_q.getQuestion()))
+	send_outputln(text, str(global_q_counter+1) + ". " + str(current_q.getQuestion()))
+
+	score_w.config(text="Score: " + str(global_score) + ", Tries Remaining: " + str(global_tries), bg="lightgray", fg="black")
+	score_w.update()
 
 # when the game ends or if the user gets a game over
 # call this method to reset all values to their defaults
@@ -71,6 +79,9 @@ def reset_everything(master, score_w, text):
 	global global_tries
 	global global_score
 	global global_q_counter
+	global begun
+
+	begun = False
 
 	# reset the global score and tries to their default values
 	global_tries = DEFAULT_TRIES
@@ -103,9 +114,9 @@ def check_input(master, score_w, text, input):
 
 	elif ( global_tries == 0 and input == "start"):
 		reset_everything(master, score_w, text)
-	elif ( not current_q.trySolution(input) ):
+	elif ( not current_q.trySolution(input) and begun):
 		give_strike(score_w)
-	elif (current_q.trySolution(input) and global_q_counter < DEF_NUM_QUESTIONS ):
+	elif (current_q.trySolution(input) and global_q_counter < DEF_NUM_QUESTIONS and begun ):
 		load_next_question(master, score_w, text)
 
 
